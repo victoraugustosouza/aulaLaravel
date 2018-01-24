48c7b137c231036e5e7ebfe804ea43bf5fd9b4cb
hey <hey@gmail.com>
2018-01-24 11:43:39 -0200

    +routes examples and html simple code

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/*
+
+Route::get('servicos', function () {
+    return "O serviço do Fulano é muito bom.<br><br>
+			Gostou?<a href='".url('contato')."'> Esse é o contato dele.</a>";
+});
+
+
+Route::get('servicos1/{id}', function ($id) {
+    return "O serviço do ".$id." é muito bom.<br><br>
+Gostou?<a href='".url('contato')."'> Esse é o contato dele.</a>";
+});
+
+
+Route::get('servicos2/{id}/{telefone}', function ($id,$telefone) {
+    return "O serviço do ".$id." é muito bom.<br><br>
+Gostou?<a href='".url('contato')."'> Esse é o telefone dele: ".$telefone.".</a>";
+});
+
+*/
